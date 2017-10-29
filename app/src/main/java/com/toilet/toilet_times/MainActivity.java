d81a98f3f2fb9d8c291fe2dc70c9cad983c53988
Chris Ayoub <chris.ayoubtexas@gmail.com>
2017-10-29 05:09:23 -0500

    updating UI

--- app/src/main/java/com/toilet/toilet_times/MainActivity.java
+++ app/src/main/java/com/toilet/toilet_times/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
