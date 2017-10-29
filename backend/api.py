81b6a0d69fcf92f1a0de8b7827ebf3927ca16707
Chris Ayoub <chris.ayoubtexas@gmail.com>
2017-10-28 21:26:29 -0500

    api on port 80

--- backend/api.py
+++ backend/api.py
@@ -194,4 +194,4 @@ api.add_resource(VotePost, '/post/vote')
 api.add_resource(DeletePost, '/post/delete')
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0', port=443)
+	app.run(debug=True, host='0.0.0.0', port=80)
