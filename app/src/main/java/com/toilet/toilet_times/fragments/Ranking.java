5e17be9d45ebe3d76e775f913808a80a128c317c
Chris Ayoub <chris.ayoubtexas@gmail.com>
2017-11-12 22:30:05 -0600

    Fixed toilet roll

--- app/src/main/java/com/toilet/toilet_times/fragments/Ranking.java
+++ app/src/main/java/com/toilet/toilet_times/fragments/Ranking.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -20,8 +22,11 @@ import com.toilet.toilet_times.R;
  */
 
 public class Ranking extends Fragment {
-//
-//    public RatingBar ratingBar;
+
+    private static final int MAX_SHEETS = 5;
+
+    public int currentRating;
+    private ImageView[] sheets = new ImageView[MAX_SHEETS];
 
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
@@ -38,20 +43,34 @@ public class Ranking extends Fragment {
         });
         b.setVisibility(View.INVISIBLE);
 
-//        RatingBar bar = rootView.findViewById(R.id.ratingBar2);
-//        ratingBar = bar;
-//        bar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-//            @Override
-//            public void onRatingChanged(RatingBar ratingBar, float v, boolean bo) {
-//                if ((int) v > 0) {
-//                    b.setVisibility(View.VISIBLE);
-//                } else {
-//                    b.setVisibility(View.INVISIBLE);
-//                }
-//            }
-//        });
+        sheets[0] = rootView.findViewById(R.id.squareOne);
+        sheets[1] = rootView.findViewById(R.id.squareTwo);
+        sheets[2] = rootView.findViewById(R.id.squareThree);
+        sheets[3] = rootView.findViewById(R.id.squareFour);
+        sheets[4] = rootView.findViewById(R.id.squareFive);
 
-//        Left arrow
+        rootView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                for (int sheet = 0; sheet < MAX_SHEETS; sheet++) {
+                    if (isEventOnTopOfView(motionEvent, sheets[sheet])) {
+                        for (int i = 0; i < MAX_SHEETS; i++) {
+                            if (i >= sheet + 1) {
+                                sheets[i].setActivated(false);
+                            } else {
+                                sheets[i].setActivated(true);
+                            }
+                        }
+                        b.setVisibility(View.VISIBLE);
+                        currentRating = sheet + 1;
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+
+        //        Left arrow
         rootView.findViewById(R.id.left_arrow_rating).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,6 +79,24 @@ public class Ranking extends Fragment {
             }
         });
         return rootView;
+    }
+
+    private boolean isEventOnTopOfView(MotionEvent event, View view) {
+        return isViewContains(view, (int) event.getRawX(), (int) event.getRawY());
+    }
+
+    private boolean isViewContains(View view, int rx, int ry) {
+        int[] location = new int[2];
+        view.getLocationOnScreen(location);
+        int x = location[0];
+        int y = location[1];
+        int w = view.getWidth();
+        int h = view.getHeight();
+
+        if (rx < x || rx > x + w || ry < y || ry > y + h) {
+            return false;
+        }
+        return true;
     }
 
 }
